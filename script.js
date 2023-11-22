4288a485d431bf4a88df8e4037da07e5078911eb
shubham-droot <shubham.verma@droot.in>
2023-11-22 19:07:04 +0530

    -- js

---
+++ script.js
@@ -0,0 +1,5 @@
+const userForm = document.getElementById("myForm");
+const userName = document.getElementById("formName");
+const pr = document.getElementById("productCategory");
+const kdls = document.getElementById("");
+const formButton = document.getElementById("formBtn");

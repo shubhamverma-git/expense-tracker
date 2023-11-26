cc026c89f2bf3b92868707a8af3e8217c2831c4e
Shubham Verma <vermashubham2707@gmail.com>
2023-11-27 01:08:18 +0530

    -- incomplete

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const formProCost = document.getElementById("userProductPrice");
 const formDlvryDate = document.getElementById("userProductDd");
 const formSubmitButton = document.getElementById("formBtn");
 
+let id = "";
+
 document.addEventListener("DOMContentLoaded", getAllData);
 
 formSubmitButton.addEventListener("click", formSubmit);
@@ -22,46 +24,48 @@ async function formSubmit(e) {
     clientProCost: formProCost.value,
     clientDlvryDate: formDlvryDate.value,
   };
-  console.log(data);
 
-  const responseData = await sendData(data);
+  const responseData = await sendData(data, id);
 
-  if (
-    responseData.clientName &&
-    responseData.clientProCat &&
-    responseData.clientPinCode &&
-    responseData.clientCountryCode &&
-    responseData.clientPhone &&
-    responseData.clientProCost &&
-    responseData.clientDlvryDate
-  ) {
-    uiCreator(responseData);
-    formUserName.value = "";
-    formProCat.value = "";
-    formDlvryPin.value = "";
-    formCountryCode.value = "";
-    formPhoneNo.value = "";
-    formProCost.value = "";
-    formDlvryDate.value = "";
-  } else {
-    alert("please fill the form");
-  }
+  uiCreator(responseData);
+  formUserName.value = "";
+  formProCat.value = "";
+  formDlvryPin.value = "";
+  formCountryCode.value = "";
+  formPhoneNo.value = "";
+  formProCost.value = "";
+  formDlvryDate.value = "";
 }
 
-async function sendData(formData) {
-  const response = await fetch(
-    "https://crudcrud.com/api/a8627ace9fdd41b8a4a12575be596264/listData",
-    {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "content-Type": "application/json",
-      },
-    }
-  );
-  const data = await response.json();
+async function sendData(formData, id) {
+  if (id == "") {
+    const response = await fetch(
+      "https://crudcrud.com/api/20872c9de2bf4f70ad5ef283ef95ad3b/listData",
+      {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
 
-  return data;
+    return data;
+  } else {
+    const response = await fetch(
+      `https://crudcrud.com/api/20872c9de2bf4f70ad5ef283ef95ad3b/listData/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(formData),
+        headers: {
+          "content-Type": "application/json",
+        },
+      }
+    );
+    // const res = await response.json();
+    // console.log(res);
+  }
 }
 
 function uiCreator(responseData) {
@@ -86,6 +90,18 @@ function uiCreator(responseData) {
   const clientProCost = document.createElement("h1");
   clientProCost.innerHTML = responseData?.clientProCost;
 
+  const editElement = document.createElement("div");
+  editElement.classList.add("control_container");
+
+  const editIcon = document.createElement("span");
+  editIcon.innerHTML = "✏️";
+
+  editIcon.addEventListener("click", () => {
+    editData(responseData);
+  });
+  editElement.appendChild(editIcon);
+  card.appendChild(editElement);
+
   const clientDlvryDate = document.createElement("h1");
   clientDlvryDate.innerHTML = responseData?.clientDlvryDate;
 
@@ -103,14 +119,29 @@ function uiCreator(responseData) {
 
 async function getAllData() {
   const receivedData = await fetch(
-    "https://crudcrud.com/api/a8627ace9fdd41b8a4a12575be596264/listData",
+    "https://crudcrud.com/api/20872c9de2bf4f70ad5ef283ef95ad3b/listData",
     {
       method: "GET",
     }
   );
   const data = await receivedData.json();
+  console.log(data);
 
   for (let i = 0; i <= data.length; i++) {
     uiCreator(data[i]);
   }
+}
+
+// Edit data
+
+async function editData(data) {
+  id = data._id;
+  formUserName.value = data.clientName;
+  formProCat.value = data.clientProCat;
+  formDlvryPin.value = data.clientPinCode;
+  formCountryCode.value = data.clientCountryCode;
+  formPhoneNo.value = data.clientPhone;
+  formProCat.value = data.clientProCat;
+  formDlvryDate.value = data.clientDlvryDate;
+  formProCost.value = data.clientProCost;
 }
